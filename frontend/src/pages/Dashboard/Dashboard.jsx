1fdecc9294bec0e10d41cf3e2d311120308ce82b
NicoleNG72 <nicolengentil@gmail.com>
2023-02-21 15:34:24 -0500

    create a table for the navbar & add process-categories

--- frontend/src/pages/Dashboard/Dashboard.jsx
+++ frontend/src/pages/Dashboard/Dashboard.jsx
@@ -5,31 +5,96 @@ function Dashboard() {
   return (
     <div className="dashboard">
       <div className="dash-nav-bar">
-        <ul>
-          <li className="dash-nav-bar-item">Dashboar</li>
-          <Link to="/" className="link dash-nav-bar-item">
-          Find Jobs 
-          </Link>
-          <li className="dash-nav-bar-item">Resume</li>
-          <Link to="/Resources" className="link dash-nav-bar-item">
-          Resources
-        </Link>
-          <li className="dash-nav-bar-item">Profile</li>
-          <li className="dash-nav-bar-item">Setting</li>
-        </ul>
+        <img src="" alt="logo" className="logo-img" />
+        <table>
+          <tr>
+            <td>
+              <img src="" alt="Icon"/>
+            </td>
+            <td>
+              <li className="dash-nav-bar-item">Dashboar</li>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img src="" alt="Icon" />
+            </td>
+            <td>
+            <Link to="/" className="link dash-nav-bar-item">
+               Find Jobs 
+            </Link>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img src="" alt="Icon" />
+            </td>
+            <td>
+              <li className="dash-nav-bar-item">Resume</li>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img src="" alt="Icon" />
+            </td>
+            <td>
+              <Link to="/Resources" className="link dash-nav-bar-item">
+                Resources
+              </Link>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img src="" alt="Icon" />
+            </td>
+            <td>
+              <Link to="/Profile" className="link dash-nav-bar-item">
+                Profile
+              </Link>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img src="" alt="Icon" />
+            </td>
+            <td>
+              <li className="dash-nav-bar-item">Setting</li>
+            </td>
+          </tr>
+        </table>
+        <button> + </button>
       </div>
 
-      <div className="container">
+
+      <div className="main-contain">
         <div className="heading">
-          <p className="heading left">hello</p>
-          <h1 className="heading middle">My Applications</h1>
-          <p className="heading right">User</p>
+          <p className="heading left">Search</p>
+          <h2 className="heading middle">My Applications</h2>
+          <a className="heading right" href="/Profile">
+            <img className="profile-img" src="" alt=""/>
+            Username
+          </a>
         </div>
-        <h2>Hello, Danny</h2>
-      </div>
-      <Link to="/" className="link">
+
+        <h1>Hello, Danny</h1>
+
+        <div className="process-categories toApply" >
+          <p>To Apply</p>
+        </div>
+        <div className="process-categories applied" >
+          <p>Applied</p>
+        </div>
+        <div className="process-categories inProgress" >
+          <p>In Progress</p>
+        </div>
+        <div className="process-categories accepted" >
+          <p>Accepted</p>
+        </div>
+        <Link to="/" className="link">
         To Home
-      </Link>
+        </Link>
+      </div>
+      
     </div>
   );
 }
