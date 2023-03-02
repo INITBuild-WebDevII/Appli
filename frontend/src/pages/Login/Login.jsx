927abbc0c5fe72c25b7fb6a6e4f8041e8bafbc68
catastrxphic <mcopo001@fiu.edu>
2023-03-02 12:13:06 -0500

    set up login form

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,14 +1,38 @@
 import { Link } from "react-router-dom";
+import './Login.css';
 
-function Login() {
+
+function LogIn() {
   return (
-    <div>
-      <h1>Login Page</h1>
+    <div className="form">
+      <h1 >Welcome Back</h1>
+      <form>
+            <div className="Login-form">
+            <div className="email">
+                <label className="instruction">Email</label><br/>
+                <input className="input" type="email" placeholder="Ex: myemail@gmail.com"  style={{ color: 'black'}}/>  
+                
+            </div>
+
+            <div className="password">
+                <label className="instruction">Password</label><br/>
+                <input className="input" type="password" placeholder="Ex: mysupersecretpassword123" style={{ color: 'black'}}/>
+            </div>
+
+            <input className="button" type="submit" value="Log In"/>
+            </div>
+            <div>
+            <p>Don't have an account? &nbsp; <Link to="/Signup" className="link"> Sign Up.</Link></p>
+
+            </div>
+    
+        </form>
       <Link to="/" className="link">
-        To Home
+          To Home
       </Link>
     </div>
   );
 }
 
-export default Login;
+export default LogIn;
+
