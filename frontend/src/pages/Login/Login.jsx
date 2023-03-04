118802d323285fd34cb4eead31aeca9a1eb2057d
catastrxphic <mcopo001@fiu.edu>
2023-03-04 10:19:25 -0500

    Login animated page

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -4,32 +4,33 @@ import './Login.css';
 
 function LogIn() {
   return (
-    <div className="form_Login">
+    <div className="Login-page">
+
       <h1 className="h1_Login" >Welcome Back</h1>
-      <form>
-            <div className="Login-form">
-            <div className="email">
-                <label className="instruction">Email</label><br/>
-                <input className="input" type="email" placeholder="Ex: myemail@gmail.com"  style={{ color: 'black'}}/>  
-                
-            </div>
+  
+          <div className="Login-email">
 
-            <div className="password">
-                <label className="instruction">Password</label><br/>
-                <input className="input" type="password" placeholder="Ex: mysupersecretpassword123" style={{ color: 'black'}}/>
-            </div>
+                <input required className="email" type="email"/>  
+                <label for="email" className="label-email">
+                  <span class="content-email">Email</span>
+                </label>
+            
+          </div>
 
-            <input className="Login_button" type="submit" value="Log In"/>
-            </div>
-            <div>
-            <p className="p_Login">Don't have an account? &nbsp; <Link to="/Signup" className="link_SU"> Sign Up.</Link></p>
+          <div className="Login-password">
 
-            </div>
-    
-        </form>
-      <Link to="/" className="link">
-          To Home
-      </Link>
+                <input required className="password" type="password"/>  
+                <label for="password" className="label-password">
+                  <span class="content-password">Password</span>
+                </label>
+
+          </div>
+
+          <input className="Login_button" type="submit" value="Log In"/>
+          <p className="p_Login">Don't have an account? &nbsp; <Link to="/Signup" className="link_LI"> Sign Up.</Link></p>
+
+          <Link to="/" className="linkLI"> To Home </Link>
+            
     </div>
   );
 }
