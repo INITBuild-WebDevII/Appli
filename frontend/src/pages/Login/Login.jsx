55d2ff18f5d704ae49f7cb886819a4e28afc697b
catastrxphic <mcopo001@fiu.edu>
2023-03-20 21:05:11 -0400

    ended styling

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -4,6 +4,8 @@ import './Login.css';
 import axios from "axios";
 import { useLogin } from "../../hooks/useLogin";
 import { useLogout } from "../../hooks/useLogout";
+import {FiMail} from "react-icons/fi";
+import {AiOutlineLock} from "react-icons/ai";
 
 function LogIn() {
   const {logout} = useLogout()
@@ -21,40 +23,55 @@ function LogIn() {
       logout()
     }
   return (
-    <form className="Login-page" onSubmit={Handlechange}>
-    <div className="Login-page">
+    <body className="body-login">
+      <form className="Login-page" onSubmit={Handlechange}>
+      <div className="Login-page">
 
-      <h1 className="h1_Login" >Welcome Back</h1>
-  
-          <div className="Login-email">
+        <h1 className="h1_Login" >Login</h1>
+    
+            <div className="Login-email">
 
-                <input name="email" onChange={(e) => setEmail(e.target.value)} required className="L-email" type="text"/>  
-                <label for="L-email" className="L-label-email">
-                  <span class="L-content-email">Email</span>
-                </label>
-            
-          </div>
+                  <input name="email" onChange={(e) => setEmail(e.target.value)} required className="L-email" type="text"/>  
+                  <label for="L-email" className="L-label-email">
+                    <span class="L-content-email"> <FiMail/> Email</span>
+                  </label>
+              
+            </div>
 
-          <div className="Login-password">
+            <div className="Login-password">
 
-                <input name="password" onChange={(e) => setPassword(e.target.value)} required className="L-password" type="password"/>  
-                <label for="L-password" className="L-label-password">
-                  <span class="L-content-password">Password</span>
-                </label>
+                  <input name="password" onChange={(e) => setPassword(e.target.value)} required className="L-password" type="password"/>  
+                  <label for="L-password" className="L-label-password">
+                    <span class="L-content-password"><AiOutlineLock/> Password </span>
+                  </label>
 
-          </div>
+            </div>
 
-          <input disable={isLoading} className="Login_button" type="submit" value="Log In"/>
-          {error && <div className="error"> {error}</div>}
-          <p className="p_Login">Don't have an account? &nbsp; <Link to="/Signup" className="link_LI"> Sign Up.</Link></p>
+            <div className="passwordDetails">
 
-          <Link to="/" className="linkLI"> To Home </Link>
-          <div>
-          <button onClick={handleClick}>Log out</button>
-          </div>
-            
-    </div>
-    </form>
+              <label className="remember-container"> 
+                <input type="checkbox" className="remember"/> Remember me
+              </label>
+
+              <label className="forgot-container"> 
+                <a className="forgot" href="resetpasswordlink">Forgot Password?</a>
+              </label>
+
+            </div>
+
+            <input disable={isLoading} className="Login_button" type="submit" value="Log In"/>
+            {error && <div className="error"> {error}</div>}
+            <p className="continue" >or continue with</p>
+            <p className="p_Login">Don't have an account? &nbsp; <Link to="/Signup" className="link_LI"> Sign Up.</Link></p>
+
+            {/* <Link to="/" className="linkLI"> To Home </Link>
+            <div>
+            <button onClick={handleClick}>Log out</button>
+            </div> */}
+              
+      </div>
+      </form>
+    </body>
   );
 }
 
