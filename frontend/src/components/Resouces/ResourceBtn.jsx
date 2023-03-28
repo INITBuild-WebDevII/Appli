664e7451f9be9783c17f10543e7fc4a4bba1343b
NicoleNG72 <nicolengentil@gmail.com>
2023-03-28 14:47:21 -0400

    Added the feature of the button popup and created a component for the resume template

--- frontend/src/components/Resouces/ResourceBtn.jsx
+++ frontend/src/components/Resouces/ResourceBtn.jsx
@@ -3,7 +3,7 @@ import "./ResourceBtn.css";
 function ResourceBtn(props) {
     return(
         <div className="resource-icon">
-            <button className="btn-icon" >
+            <button onClick={props.click} className="btn-icon" >
                 <img className="resource-img" src={props.src} alt={props.title}/>
             </button>
             <p className="btn-text">{props.title}</p>
--- frontend/src/pages/Resources/Resources.jsx
+++ frontend/src/pages/Resources/Resources.jsx
@@ -2,16 +2,21 @@ import { Link } from "react-router-dom";
 import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
 import ResourceBtn from "../../components/Resouces/ResourceBtn";
+import ResumeTemplate from "../../components/Resouces/ResumeTemplate";
 import "./Resources.css";
 import strong from "../../assets/strong.png"
 import resumeicon from "../../assets/resumeicon.png"
 import technicalicon from "../../assets/technicalicon.png"
 import behavioralicon from "../../assets/behavioralicon.png"
+import { useState } from "react";
 
 function Resources() {
+  const [material, setMaterial] = useState("resumeTemplate")
+
+
   return (
     <div  className="resources">
-        <Navbar />
+      <Navbar />
       <div className="content">
         <div>
           <img className="strong" src={strong} alt="strong"/>
@@ -21,13 +26,21 @@ function Resources() {
           </div>
         </div>
         <div className="resource-icons">
-          <ResourceBtn title="Resume Template"  src={resumeicon}/>
-          <ResourceBtn title="Technical Question"  src={technicalicon}/>
-          <ResourceBtn title="Behavioral Question"  src={behavioralicon}/>
+
+          <ResourceBtn click={()=>setMaterial(`resumeTemplate`)} title="Resume Template"  src={resumeicon} />
+          <ResourceBtn click={()=>setMaterial(`technicalQuestion`)} title="Technical Question"  src={technicalicon}/>
+          <ResourceBtn click={()=>setMaterial(`behavioralQuestion`)} title="Behavioral Question"  src={behavioralicon} />
+        
+        </div>
+        <div className="interview-resources">
+            {material === "resumeTemplate" && <ResumeTemplate />}
+            {/* {material === "technicalQuestion"}
+            {material === "behavioralQuestion"} */}
+            
         </div>
       </div>
-        <Footer />
-        <Link to="/" className="link">To Home</Link>
+      <Footer />
+      <Link to="/" className="link">To Home</Link>
     </div>
   );
 }
