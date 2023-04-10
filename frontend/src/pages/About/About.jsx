d773a04fc7fcdfb8bbd44d53699331d7ddb71287
MIGUEL <msabl005@fiu.edu>
2023-04-10 18:25:08 -0400

    Started about page

--- frontend/src/pages/About/About.jsx
+++ frontend/src/pages/About/About.jsx
@@ -1,9 +1,21 @@
 import { Link } from "react-router-dom";
+import "./About.css";
+import Navbar from "../../components/Navbar";
 
 function About() {
   return (
-    <div>
-      <h1>About Us Page</h1>
+    <div className="about">
+      <Navbar/>
+      <div className="about-banner">
+        <h1>About Us</h1>
+      </div>
+      {/*
+      <div className="members-container">
+        <div className="member-card">
+          <h1>Miguel</h1>
+          <div>Product Owner</div>
+        </div>
+  </div>*/}
       <Link to="/" className="link">
         To Home
       </Link>
